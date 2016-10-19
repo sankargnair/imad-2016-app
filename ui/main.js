a62ea308d526cbd004742096d1bbbbf4d4a7d3dc
sankargnair <sgnair77@gmail.com>
2016-10-19 12:45:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,5 +2,11 @@ console.log('Loaded!');
 var img=document.getElementById('img1');
 
 img.onclick =function(){
-    img.style.marginLeft='300px';
+    var interval=setInterval(moveRight,100);
+    //img.style.marginLeft='300px';
+}
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft=marginLeft+ 'px';
 }
