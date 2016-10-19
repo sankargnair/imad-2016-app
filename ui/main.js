05e87e5d0669a6602684650152afd9845a63d92e
sankargnair <sgnair77@gmail.com>
2016-10-19 16:08:47 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,3 +28,31 @@ button.onclick= function()
    request.send(null);
 };
     
+//submit name
+var nameInput=document.getElementById('name');
+var name1= nameInput.value;
+var submit =document.getElementById('submit_btn');
+submit.onclick=function(){
+    
+    //make a request to server and send name
+    
+    
+    //capture a list of names and render it as a list
+    var names =["name1","name2","name3","name4"];
+    var list= '';
+    for (var i=0; i<names.length; i++)
+    {
+        list += '<li>' + names[i] + '</li>';
+        
+    }
+    var ul = document.getElementById('namelist');
+    ul.innerHTML=list;
+};
+
+    
+    
+    
+    
+    
+
+
