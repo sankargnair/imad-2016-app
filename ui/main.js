c55272205fc3d8e2358111591faedcad30b38ee7
sankargnair <sgnair77@gmail.com>
2016-10-19 14:34:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,4 @@
-console.log('Loaded!');
-var img=document.getElementById('img1');
 
-img.onclick =function(){
-    var interval=setInterval(moveRight,100);
-    //img.style.marginLeft='300px';
-}
-var marginLeft=0;
-function moveRight(){
-    marginLeft=marginLeft+5;
-    img.style.marginLeft=marginLeft+ 'px';
-}
 
 var button=document.getElementById('counter');
 var counter=0;
@@ -23,7 +12,7 @@ button onclick= function()
     
     
     //render the variable in the span
-    counter=counter+1;
+    counter=counter + 1;
     var span=document.getElementById('count');
     span.innerHTML= counter.toString();
 }
